f8db5df549611f4d6d470818aa22355326e0b711
HyogaGlacier <hyoga_quasar@yahoo.co.jp>
2019-01-11 16:13:34 +0900

    update kadai04.py

--- programming/kadai04.py
+++ programming/kadai04.py
@@ -5,13 +5,13 @@ import numpy as np
 step = 1000
 x = []
 y = []
-for i in range(10):
-  q = i / 10
+for i in range(100):
+  q = i / 100
   proc = subprocess.run(["./random_walk", str(q), str(step)], stdout=subprocess.PIPE)
   x.append(q)
   y.append(int(proc.stdout.decode("utf8")) / step)
 
-plt.plot(x, y)
+plt.scatter(x, y)
 x = np.linspace(0, 1, 1000)
 y = x / (x + 1)
 plt.plot(x, y)
